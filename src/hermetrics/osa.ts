b85cdc498baf6e4e1330ff76e8bc6aca84406b02
juan <juan@pop-os.localdomain>
2020-03-11 15:42:43 -0600

    finalized osa distance and their test scripts

--- src/hermetrics/osa.ts
+++ src/hermetrics/osa.ts
@@ -22,63 +22,46 @@ class OSA extends Levenshtein
         const rows: number = sourceLength + 1;
         const cols: number = targetLength + 1;
 
-                const distanceMatrix: number[][] = Array<number>(rows).fill(0).map(() => Array<number>(cols).fill(0))
+        const distanceMatrix: number[][] = Array<number>(rows).fill(0).map(() => Array<number>(cols).fill(0))
 
-        for(var i = 0; i < rows; i++)
+        for(let i = 1; i < rows; i++)
         {
-            distanceMatrix[i][0] = UPPER;
+            distanceMatrix[i][0] = i*removeCost;
         }
-        for(var i = 0; i < cols; i++)
-        {
-            distanceMatrix[0][i] = UPPER;
-        }
-        for(var i = 1; i < rows; i++)
-        {
-            distanceMatrix[i][1] = (i - 1)*insertCost;
-        }
-        for(var i = 1; i < cols; i++)
-        {
-            distanceMatrix[1][i] = (i - 1)*insertCost;
-        }
-        
-        var lastMatchCol : number = 0;
-        var lastMatchRow : number = 0;
-        var sourceSymbol : string = "";
-        var targetSymbol : string = "";
-        var lastRow  = Object.create(null);
-        var optSubCost : number = 0;
-        var deletion : number = 0;
-        var insertion : number = 0;
-        var substitution : number = 0; 
-        var transpotition : number = 0;
-        for(var i = 1; i < sourceLength + 1; i++)
-        {
-            sourceSymbol = source[i - 1];
-            lastMatchCol = 0;
 
-            for( var j = 1; j < targetLength + 1; j++)
+        for(let i = 1; i < cols; i++)
+        {
+            distanceMatrix[0][i] = i*insertCost;
+        }
+
+   
+        let deletion: number = 0
+        let insertion: number = 0
+        let substitution: number = 0
+
+
+        for(let i = 1; i < rows; i++)
+        {
+            for(let j = 1; j < cols; j++)
             {
-                targetSymbol = target[j-1]
-                lastMatchRow = lastRow[targetSymbol] != undefined ? lastRow[targetSymbol] : 0;
-                optSubCost =  sourceSymbol == targetSymbol ? 0  : subtractCost;
+                deletion = distanceMatrix[i - 1][j] + removeCost;
+                insertion = distanceMatrix[i][j - 1] + insertCost;
+                substitution = distanceMatrix[i - 1][j - 1];
 
-                deletion = distanceMatrix[i][j + 1] + removeCost;
-                insertion = distanceMatrix[i + 1][j] + insertCost;
-                substitution = distanceMatrix[i][j] + optSubCost;
-
-                transpotition = distanceMatrix[lastMatchRow][lastMatchCol] + 
-                                Math.max((i - lastMatchRow)*removeCost, (j - lastMatchCol) * insertCost) + transposCost;
-                distanceMatrix[i + 1][j + 1] = Math.min(deletion, insertion, substitution, transpotition);
-
-                if (optSubCost == 0)
+                if(source[i - 1] != target[j - 1])
                 {
-                    lastMatchCol = j;
+                    substitution += subtractCost;
+                }
+                
+                distanceMatrix[i][j] = Math.min(deletion, insertion, substitution);
+
+                if(i > 1 && j > 1 && source[i - 1] == target[j - 2] && source[i - 2] == target[j - 1])
+                {
+                    distanceMatrix[i][j] = Math.min(distanceMatrix[i][j], distanceMatrix[i - 2][j - 2] + transposCost);
                 }
             }
-            lastRow[sourceSymbol] = i;
         }
 
-        //this.printMatrix(distanceMatrix)
         return distanceMatrix[rows-1][cols-1];
     }
     private printMatrix(distanceMatrix: number[][] ) : void
@@ -97,6 +80,31 @@ class OSA extends Levenshtein
     public maxDistance(source: string, target: string, { deletionCost, insertionCost, substitutionCost, cost=1 } : LevenshteinCostOptions = {}): number
     {
         return super.maxDistance(source, target, { deletionCost, insertionCost, substitutionCost, cost})
+    }
+    public maxDistanceWithTranspositions(source: string, target: string, { deletionCost, insertionCost, substitutionCost, transpositionCost, cost=1 } : LevenshteinCostOptions = {}): number
+    {
+        const sourceLength: number = source.length;
+        const targetLength: number = target.length;
+        let removeCost: number = deletionCost ??  cost;
+        let insertCost: number = insertionCost ?? cost;
+        let subtractCost: number = substitutionCost ?? cost;
+        let transposCost: number = transpositionCost ?? cost;
+
+        subtractCost = Math.min(subtractCost, removeCost + insertCost)
+        transposCost = Math.min(transposCost, subtractCost*2);
+
+        const maxDel : number = Math.max(sourceLength - targetLength, 0);
+        const maxIns : number = Math.max(targetLength - sourceLength, 0);
+        const maxSub : number = Math.min(sourceLength, targetLength)        ;
+        const maxTra : number = Math.trunc(maxSub / 2);
+        const extraSub : number = maxSub % 2;
+
+        const delDist : number = maxDel * removeCost;
+        const insDist : number = maxIns * insertCost;
+        const subDist : number = maxSub * subtractCost;
+        const traDist : number = maxTra * transposCost;
+
+        return delDist + insDist + Math.min(subDist, traDist);
     }
 }
 
